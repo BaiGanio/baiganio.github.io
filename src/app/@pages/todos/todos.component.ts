ae57b5bf3e15add78ed4109d2c10301a7eaa8fac
Lyuben Kikov <exceptionhelper@gmail.com>
2021-10-14 21:45:44 +0300

    Basic httpget implementation in TodosComponent.

--- src/app/@pages/todos/todos.component.ts
+++ src/app/@pages/todos/todos.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ToDo } from 'src/app/@shared/models/todo.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
+  todos = new Array<ToDo>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>(environment.todoApiUrl).subscribe({
+      next: data => {
+        this.todos = data;
+        console.log(this.todos);
+      },
+      error: error => {
+          console.error('There was an error!', error.message);
+      }
+    })
   }
 
 }
