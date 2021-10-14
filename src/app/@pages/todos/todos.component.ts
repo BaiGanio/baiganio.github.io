0625bfbcd3c55b35b6fccff4f8762c43e3af4c7a
Lyuben Kikov <exceptionhelper@gmail.com>
2021-10-14 22:34:34 +0300

    Implemented basic table

--- src/app/@pages/todos/todos.component.ts
+++ src/app/@pages/todos/todos.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ToDo } from 'src/app/@shared/models/todo.model';
 import { environment } from 'src/environments/environment';
 
@@ -10,19 +13,65 @@ import { environment } from 'src/environments/environment';
 })
 export class TodosComponent implements OnInit {
   todos = new Array<ToDo>();
+  loading = false;
+  expandedElement: ToDo | null;
+
+  public dataSource: MatTableDataSource<ToDo>;
+  displayedColumns: string[] =
+  [
+    '#', 'Id', 'Name', 'Description', 'Date', 'Is Done', 'Actions'
+  ];
 
   constructor(private http: HttpClient) { }
 
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+
   ngOnInit(): void {
+    this.loading = true;
     this.http.get<any>(environment.todoApiUrl).subscribe({
       next: data => {
-        this.todos = data;
+
+        console.log(data);
+        data.forEach(element => {
+          const c = {
+            Id: element.id,
+            Name: element.name,
+            Description: element.description,
+            Date: element.date,
+            IsDone: element.isDone,
+          };
+          this.todos.push(c as ToDo);
+        });
+        this.dataSource = new MatTableDataSource(this.todos);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.loading = false;
         console.log(this.todos);
       },
       error: error => {
           console.error('There was an error!', error.message);
       }
     })
+  }
+
+  // Search in data table based on the input fields that're used from filters group
+  applyFilter() {
+    const teacherNameFilterValue = ( document.querySelector('#tiFilter') as HTMLInputElement).value;
+    const courseNameFilterValue = ( document.querySelector('#nameFilter') as HTMLInputElement).value;
+
+    this.dataSource.filterPredicate = (data, filter) =>
+      (
+        // data.TeacherName.trim().toLowerCase().indexOf(filter.split('┼')[0]) !== -1 &&
+        data.Name.trim().toLowerCase().indexOf(filter.split('┼')[1]) !== -1
+      );
+
+    this.dataSource.filter =
+    `${teacherNameFilterValue.trim().toLowerCase()}┼${courseNameFilterValue.trim()}`;
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
