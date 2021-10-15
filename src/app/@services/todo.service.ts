9408430e3cf55b0f5910b625f06a1003403c5a46
Lyuben Kikov <exceptionhelper@gmail.com>
2021-10-15 23:26:24 +0300

    Polished todo final looks

--- src/app/@services/todo.service.ts
+++ src/app/@services/todo.service.ts
@@ -3,26 +3,27 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { ToDo } from "../@shared/models/todo.model";
+import { BackendService } from "./backend.service";
 
 
 @Injectable()
 export class ToDoService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private backendService: BackendService) { }
 
     getAll(): Observable<any> {
       return this.http.get(environment.todoApiUrl);
     }
 
     create(data: ToDo): Observable<any> {
-      return this.http.post(environment.todoApiUrl + 'create', data);
+      return this.http.post(environment.todoApiUrl + 'create', data, {'headers': this.backendService.getContentHeaders(false) });
     }
 
     update(data: ToDo): Observable<any> {
-      return this.http.put(environment.todoApiUrl + 'update', data);
+      return this.http.put(environment.todoApiUrl + 'update', data, {'headers': this.backendService.getContentHeaders(false) });
     }
 
     delete(id: number): Observable<any> {
-      return this.http.delete(environment.todoApiUrl + id);
+      return this.http.delete(environment.todoApiUrl + id, {'headers': this.backendService.getContentHeaders(false) });
     }
 }
