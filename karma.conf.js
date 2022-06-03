65854abad7ac794adf1fecbebaa848ca0fdc5541
Lyuben Kikov <exceptionhelper@gmail.com>
2022-06-03 15:51:07 +0300

    ...

--- karma.conf.js
+++ karma.conf.js
@@ -42,6 +42,12 @@ module.exports = function (config) {
                 functions: 75,
                 lines: 75,
             }
+        },
+        watermarks: {
+          statements: [ 50, 80 ],
+          functions: [ 50, 80 ],
+          branches: [ 50, 80 ],
+          lines: [ 50, 80 ]
         }
       },
       reporters: ['progress', 'kjhtml'],
