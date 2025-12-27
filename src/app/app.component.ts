3d2dba35342519ccfb024bfb2fd495c6570cfb66
Lyuben Kikov <exceptionhelper@gmail.com>
2025-12-27 17:33:52 +0200

    wtf e...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button'; //
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [MatButtonModule],
+  template: `<button matButton="elevated">Click Me</button>`
 })
 export class AppComponent {
   title = 'baiganio.github.io';
